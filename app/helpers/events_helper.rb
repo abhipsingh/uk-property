485ed08f919df23259185828370a906dbb119ca2
John Doe <residentevil293@gmail.com>
2016-11-04 14:31:59 +0000

    safely handle unwanted searches for db insertions

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,28 +1,38 @@
 module EventsHelper
 
   def insert_events(agent_id, property_id, buyer_id, message, type_of_match, property_status_type, event)
-    session = Rails.configuration.cassandra_session
-    date = Date.today.to_s
-    month = Date.today.month
-    time = Time.now.strftime("%Y-%m-%d %H:%M:%S").to_s
-    cqls = [
-            "INSERT INTO Simple.property_events_buyers_events (stored_time, time_of_event, date, property_id, status_id, buyer_id, event, message, type_of_match, month) VALUES ('#{time}', now(), '#{date}', '#{property_id}', #{property_status_type}, #{buyer_id}, #{event}, '#{message}', #{type_of_match}, #{month});",
-            "INSERT INTO Simple.agents_buyer_events (stored_time, time_of_event, agent_id, property_id, status_id, buyer_id, event, message, type_of_match) VALUES ( '#{time}', now(), #{agent_id}, '#{property_id}', #{property_status_type}, #{buyer_id}, #{event}, '#{message}', #{type_of_match});",
-            "INSERT INTO Simple.timestamped_property_events (stored_time, time_of_event, agent_id, property_id, status_id, buyer_id, event, message, type_of_match) VALUES ( '#{time}', now(), #{agent_id}, '#{property_id}', #{property_status_type}, #{buyer_id},  #{event}, '#{message}', #{type_of_match});",
-            "INSERT INTO Simple.buyer_property_events (stored_time, date, buyer_id, property_id, status_id, event, message, type_of_match, month) VALUES ( '#{time}', '#{date}', #{buyer_id}, '#{property_id}', #{property_status_type}, #{event}, '#{message}', #{type_of_match}, #{month});"
-          ]
+    agent_id = agent_id.to_i
+    property_id = property_id.to_i
+    buyer_id = buyer_id.to_i
+    type_of_match = type_of_match.to_i
+    property_status_type = property_status_type.to_i
+    event = event.to_i
+    #### Defend against null cases
+    if agent_id && property_id && buyer_id && type_of_match && property_status_type && event
+      
+      session = Rails.configuration.cassandra_session
+      date = Date.today.to_s
+      month = Date.today.month
+      time = Time.now.strftime("%Y-%m-%d %H:%M:%S").to_s
+      cqls = [
+              "INSERT INTO Simple.property_events_buyers_events (stored_time, time_of_event, date, property_id, status_id, buyer_id, event, message, type_of_match, month) VALUES ('#{time}', now(), '#{date}', '#{property_id}', #{property_status_type}, #{buyer_id}, #{event}, '#{message}', #{type_of_match}, #{month});",
+              "INSERT INTO Simple.agents_buyer_events (stored_time, time_of_event, agent_id, property_id, status_id, buyer_id, event, message, type_of_match) VALUES ( '#{time}', now(), #{agent_id}, '#{property_id}', #{property_status_type}, #{buyer_id}, #{event}, '#{message}', #{type_of_match});",
+              "INSERT INTO Simple.timestamped_property_events (stored_time, time_of_event, agent_id, property_id, status_id, buyer_id, event, message, type_of_match) VALUES ( '#{time}', now(), #{agent_id}, '#{property_id}', #{property_status_type}, #{buyer_id},  #{event}, '#{message}', #{type_of_match});",
+              "INSERT INTO Simple.buyer_property_events (stored_time, date, buyer_id, property_id, status_id, event, message, type_of_match, month) VALUES ( '#{time}', '#{date}', #{buyer_id}, '#{property_id}', #{property_status_type}, #{event}, '#{message}', #{type_of_match}, #{month});"
+            ]
 
-    cqls.map { |e| Rails.logger.info(e)  }
+      cqls.map { |e| Rails.logger.info(e)  }
 
-    cqls.map { |each_cql| session.execute(each_cql)  }
+      cqls.map { |each_cql| session.execute(each_cql)  }
 
-    response = {}
+      response = {}
 
-    if event == Trackers::Buyer::EVENTS[:sold]
-      host = Rails.configuration.remote_es_host
-      client = Elasticsearch::Client.new host: host
-      response = client.update index: 'addresses', type: 'address', id: property_id.to_s,
-                        body: { doc: { property_status_type: 'Red', vendor_id: buyer_id } }
+      if event == Trackers::Buyer::EVENTS[:sold]
+        host = Rails.configuration.remote_es_host
+        client = Elasticsearch::Client.new host: host
+        response = client.update index: 'addresses', type: 'address', id: property_id.to_s,
+                          body: { doc: { property_status_type: 'Red', vendor_id: buyer_id } }
+      end
     end
     response
   end
