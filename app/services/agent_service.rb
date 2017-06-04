9e81f5cac455d18b998cd825c9ba7847adf4320a
John Doe <residentevil293@gmail.com>
2017-06-05 02:04:50 +0530

    minor refactor for agents verification workflow

--- app/services/agent_service.rb
+++ app/services/agent_service.rb
@@ -9,28 +9,16 @@ class AgentService
   ### When an agent verifies a crawled property
   ### Called from agents_controller#verify_property_from_agent
   def verify_crawled_property_from_agent(property_attrs, vendor_email, assigned_agent_email)
-    client = Elasticsearch::Client.new host: Rails.configuration.remote_es_host
-    assigned_agent = Agents::Branches::AssignedAgent.where(email: assigned_agent_email).first
-    branch = Agents::Branches::AssignedAgent.where(id: @agent_id.to_i).last.branch
-    if assigned_agent
-      property_attrs[:agent_id] = assigned_agent.id
-      property_attrs[:agent_status] = 2
-      assigned_agent_present = true
-    else
-      InvitedAgent.create!(email: assigned_agent_email, udprn: udprn)
-      branch.invited_agents = [{ branch_id: branch.id, company_id: branch.agent_id, email: assigned_agent_email }]
-      branch.save!
-      branch.send_emails
-      assigned_agent_present = false
-    end
-    property_id = property_attrs[:property_id]
+    response, status = verify_property_from_agent(property_attrs, vendor_email, assigned_agent_email)
     Agents::Branches::CrawledProperty.where(id: property_id).update_all({udprn: udprn})
-    response, status = PropertyDetails.update_details(client, udprn, property_attrs)
-    Agents::Branches::AssignedAgent.find(@agent_id).send_vendor_email(vendor_email, @udprn, assigned_agent_present, assigned_agent_email)
     return response, status
   end
 
   def verify_manual_property_from_agent(property_attrs, vendor_email, assigned_agent_email)
+    verify_property_from_agent(property_attrs, vendor_email, assigned_agent_email)
+  end
+
+  def verify_property_from_agent(property_attrs, vendor_email, assigned_agent_email)
     client = Elasticsearch::Client.new host: Rails.configuration.remote_es_host
     assigned_agent = Agents::Branches::AssignedAgent.where(email: assigned_agent_email).first
     branch = Agents::Branches::AssignedAgent.where(id: @agent_id.to_i).last.branch
