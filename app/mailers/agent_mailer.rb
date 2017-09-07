76182e8ae4005df789ab4b168d9711cedb6352e9
Resident Evil <residentevil293@gmail.com>
2017-09-07 20:41:23 +0530

    Send emails to admin and previous agent when assigned agent is changed for a property

--- app/mailers/agent_mailer.rb
+++ app/mailers/agent_mailer.rb
@@ -5,4 +5,76 @@ class AgentMailer < ApplicationMailer
     mail(to: user.agent_email, subject: "Welcome to Prophety")
   end
 
+
+  def send_email_on_assigned_agent_change_to_previous_agent(agent, property_details, vendor, reason, time)
+    branch = agent.branch
+    company = branch.agent
+    group = company.group
+
+    @branch_name = branch.name
+    @company_name = company.name
+    @group_name = group.name
+    @agent_image_url = agent.image_url
+    @agent_first_name = agent.first_name
+    @agent_last_name = agent.last_name
+    @agent_title = agent.title
+    @agent_mobile = agent.mobile
+    @agent_office_number = agent.office_phone_number
+    @udprn = property_details['udprn']
+    @address = property_details['address']
+    @vendor_first_name = vendor.first_name
+    @vendor_last_name = vendor.last_name
+    @vendor_image_url = vendor.image_url
+    @vendor_email = vendor.email
+    @vendor_mobile = vendor.mobile
+    @reason = reason
+    @time = time
+    mail(to: agent.email, subject: "Change Agent Request")
+  end
+
+  def send_email_on_assigned_agent_change_to_admin(previous_agent, new_agent, property_details, vendor, reason, time)
+    previous_branch = previous_agent.branch
+    previous_company = previous_branch.agent
+    previous_group = previous_company.group
+
+    branch = new_agent.branch
+    company = new_agent.agent
+    group = new_agent.group
+
+    @new_branch_name = branch.name
+    @new_company_name = company.name
+    @new_group_name = group.name
+
+    @prev_branch_name = previous_branch.name
+    @prev_company_name = previous_company.name
+    @prev_group_name = previous_group.name
+
+    @previous_agent_image_url = previous_agent.image_url
+    @previous_agent_first_name = previous_agent.first_name
+    @previous_agent_last_name = previous_agent.last_name
+    @previous_agent_title = previous_agent.title
+    @previous_agent_mobile = previous_agent.mobile
+    @previous_agent_office_number = previous_agent.office_phone_number
+
+    @new_agent_image_url = new_agent.image_url
+    @new_agent_first_name = new_agent.first_name
+    @new_agent_last_name = new_agent.last_name
+    @new_agent_title = new_agent.title
+    @new_agent_mobile = new_agent.mobile
+    @new_agent_office_number = new_agent.office_phone_number
+
+    @udprn = property_details['udprn']
+    @address = property_details['address']
+    @vendor_first_name = vendor.first_name
+    @vendor_last_name = vendor.last_name
+    @vendor_image_url = vendor.image_url
+    @vendor_email = vendor.email
+    @vendor_mobile = vendor.mobile
+    @reason = reason
+    @time = time
+    
+    admin_email = 'test@prophety.co.uk'
+    mail(to: admin_email, subject: "Change of Agent")
+  end
+
 end
