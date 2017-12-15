eecc26704d191929d60d96c0b20fdc1d152b925b
John Doe <residentevil293@gmail.com>
2017-12-15 13:41:04 +0000

    agent now still remaining attached to the property after the property being sold except for developers

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -115,8 +115,17 @@ module EventsHelper
           SoldProperty.create!(udprn: property_id, buyer_id: buyer_id, agent_id: agent_id, vendor_id: vendor_id, sale_price: message[:final_price], completion_date: message[:completion_date])
 
           ### Update the property details
-          update_hash = { property_status_type: nil, vendor_id: new_vendor_id , sold: true, claimed_on: Time.now.to_s, claimed_by: 'Vendor', agent_id: nil }
-          response = PropertyService.new(property_id).update_details(update_hash)
+          update_hash = { property_status_type: nil, vendor_id: new_vendor_id , sold: true, claimed_on: Time.now.to_s, claimed_by: 'Vendor' }
+
+          ### Create a lead for local agents if a new property is sold
+          if details[:is_developer].to_s == 'true'
+            update_hash[:agent_id] = nil
+            property_service = PropertyService.new(property_id)
+            property_service.attach_vendor_to_property(new_vendor_id.to_i)
+            response = property_service.update_details(update_hash)
+          else
+            response = PropertyService.new(property_id).update_details(update_hash)
+          end
   
           ### Archive the enquiries that were received for this property
           Event.where(udprn: property_id).where(is_archived: false).update_all(is_archived: true)
