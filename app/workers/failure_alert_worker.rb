ac7ab09de1c0384d1f3cea54e6b583febecb0766
EC2 Default User <ec2-user@ip-172-31-32-239.eu-west-2.compute.internal>
2018-06-12 13:50:29 +0000

    failure alerts

--- app/workers/failure_alert_worker.rb
+++ app/workers/failure_alert_worker.rb
@@ -3,9 +3,9 @@ class FailureAlertWorker
   sidekiq_options :retry => false # job will be discarded immediately if failed
 
   def perform(exception, route)
-  	Rails.configuration.failed_request_counter.observe(1, { route: route, exception: exception })
-    sleep 3
-  	Rails.configuration.failed_request_counter.observe(1, { route: route, exception: exception })
+    Rails.configuration.failed_request_counter.observe(1, { route: route, exception: exception })
+    sleep 5
+    Rails.configuration.failed_request_counter.observe(1, { route: route, exception: exception })
   end
 end
 
